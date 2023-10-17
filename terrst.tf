70c98768814203ab4c310fa0ad0ced201cc12616
Shaikthouhid <141808860+Shaikthouhid@users.noreply.github.com>
2023-10-18 00:31:48 +0530

    Update terrst.tf

--- terrst.tf
+++ terrst.tf
@@ -1,7 +1,7 @@
 provider "aws" {
 region = "us-east-1"
-access_key = "AKIATLBBZI56TW2EUPQI"
-secret_key = "dqXxjd7GmzWjbDHffOD1IkJIe2QO5L8jxfetTUHnl"
+access_key = "AKIATLBBZI56QWJVN56O"
+secret_key = "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"
 }
 resource "aws_s3_bucket" "s3buckett" {
  bucket = "shaiktouhidd62"
